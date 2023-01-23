2c7b7b9f574b0bf6b6513c45aa3f34e2ab0cb3f8
SATANalexander666 <10b1111nika@gmail.com>
2023-01-23 17:28:52 +0300

    fuck

---
+++ main/.build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/axr/prog/projects/crypto-monitor/main/.build/app_autogen/mocs_compilation.cpp" "CMakeFiles/app.dir/app_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/app.dir/app_autogen/mocs_compilation.cpp.o.d"
+  "/home/axr/prog/projects/crypto-monitor/main/main.cpp" "CMakeFiles/app.dir/main.cpp.o" "gcc" "CMakeFiles/app.dir/main.cpp.o.d"
+  "/home/axr/prog/projects/crypto-monitor/main/src/DataManager.cpp" "CMakeFiles/app.dir/src/DataManager.cpp.o" "gcc" "CMakeFiles/app.dir/src/DataManager.cpp.o.d"
+  "/home/axr/prog/projects/crypto-monitor/main/src/MainWindow.cpp" "CMakeFiles/app.dir/src/MainWindow.cpp.o" "gcc" "CMakeFiles/app.dir/src/MainWindow.cpp.o.d"
+  "/home/axr/prog/projects/crypto-monitor/main/src/Reciever.cpp" "CMakeFiles/app.dir/src/Reciever.cpp.o" "gcc" "CMakeFiles/app.dir/src/Reciever.cpp.o.d"
+  "/home/axr/prog/projects/crypto-monitor/main/src/TableLinker.cpp" "CMakeFiles/app.dir/src/TableLinker.cpp.o" "gcc" "CMakeFiles/app.dir/src/TableLinker.cpp.o.d"
+  "/home/axr/prog/projects/crypto-monitor/main/src/TableModel.cpp" "CMakeFiles/app.dir/src/TableModel.cpp.o" "gcc" "CMakeFiles/app.dir/src/TableModel.cpp.o.d"
+  "/home/axr/prog/projects/crypto-monitor/main/src/TableView.cpp" "CMakeFiles/app.dir/src/TableView.cpp.o" "gcc" "CMakeFiles/app.dir/src/TableView.cpp.o.d"
+  "/home/axr/prog/projects/crypto-monitor/main/src/ToolBar.cpp" "CMakeFiles/app.dir/src/ToolBar.cpp.o" "gcc" "CMakeFiles/app.dir/src/ToolBar.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main/.build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/app_autogen.dir/ParseCache.txt"
+  "app_autogen"
+  "CMakeFiles/app.dir/app_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/app.dir/app_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/app.dir/main.cpp.o"
+  "CMakeFiles/app.dir/main.cpp.o.d"
+  "CMakeFiles/app.dir/src/DataManager.cpp.o"
+  "CMakeFiles/app.dir/src/DataManager.cpp.o.d"
+  "CMakeFiles/app.dir/src/MainWindow.cpp.o"
+  "CMakeFiles/app.dir/src/MainWindow.cpp.o.d"
+  "CMakeFiles/app.dir/src/Reciever.cpp.o"
+  "CMakeFiles/app.dir/src/Reciever.cpp.o.d"
+  "CMakeFiles/app.dir/src/TableLinker.cpp.o"
+  "CMakeFiles/app.dir/src/TableLinker.cpp.o.d"
+  "CMakeFiles/app.dir/src/TableModel.cpp.o"
+  "CMakeFiles/app.dir/src/TableModel.cpp.o.d"
+  "CMakeFiles/app.dir/src/TableView.cpp.o"
+  "CMakeFiles/app.dir/src/TableView.cpp.o.d"
+  "CMakeFiles/app.dir/src/ToolBar.cpp.o"
+  "CMakeFiles/app.dir/src/ToolBar.cpp.o.d"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
